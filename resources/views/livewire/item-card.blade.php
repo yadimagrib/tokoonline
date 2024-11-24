5356c1cef82c63d7515350a602a9a84d1b19b3e0
yadimagrib <mulyadisopiyani@gmail.com>
2024-11-24 23:38:46 +0700

    instalasi laravel

---
+++ resources/views/livewire/item-card.blade.php
@@ -0,0 +1,19 @@
+<div class="bg-gray-100 shadow-sm rounded-lg">
+    <div>
+        <img src="{{ asset('images/1.jpg')}}" alt="product-images" class="rounded-t-lg object-cover" height="400px" width="400px">
+    </div>
+    <div>
+        <h2 class="line-clamp-2 px-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
+            numquam ea!</h2>
+            <div class="flex justify-between px-3 py-2">
+                <div  class="bg-blue-200 p-1 rounded-md">
+                    <h2 class="text-1xl">Outfit</h2>
+                </div>
+                <h2 class="text-1xl font-medium">Rp.300.000</h2>
+            </div>
+            <div class="px-3">
+
+                <a class="block w-full rounded bg-green-600 px-12 py-2 text-center text-sm font-medium text-white shadow hover:bg-green-700 focus:outline-none focus:ring active-bg-green-500 sm:w-auto"  href=""> Add to cart</a>
+            </div>
+    </div>
+</div>
---
+++ resources/views/livewire/product-section.blade.php
@@ -0,0 +1,12 @@
+<div class='px-10 md:px-20 sm:px-30'>
+    <h2 class='font-medium text-[20px] my-3'>Brand New</h2>
+    <livewire:product-listing />
+
+    <h2 class='font-medium text-[20px] my-3'>Smartphone & Laptop</h2>
+    <livewire:product-listing />
+
+    <h2 class='font-medium text-[20px] my-3'>Outfits</h2>
+    <livewire:product-listing />
+    <h2 class='font-medium text-[20px] my-3'>Digital Product</h2>
+    <livewire:product-listing />
+</div>
